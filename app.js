9a160024f82606bc83ee105fc180b800f4a29aca
Tanuris Pina <tanurispina@gmail.com>
2025-08-05 17:08:10 -0300

    feat(js): seleciona elementos do DOM e inicializa variáveis

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Retirei os onclick do botao no html e recebi os dados via dom, adicionando addEventListener
+// Lista que armazena os nomes
+let amigos = [];
+
+// Elementos do DOM
+const nomeDoAmigo = document.querySelector("#amigo");
+const btnAdicionar = document.querySelector("#send");
+const listaAmigos = document.querySelector("#listaAmigos");
+const mensagemSorteio = document.querySelector(".result-list");
+const btnSortear = document.querySelector("#sortear");
+const btnReiniciar = document.querySelector("#reiniciar");
